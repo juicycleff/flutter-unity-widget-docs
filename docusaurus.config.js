cd688c4fa405eeaf5af3b81401dcab5d946a9144
Rex Isaac Raphael <rex.raphael@outlook.com>
2021-02-22 19:37:35 +0100

    [docs] updated sidebar config

---
+++ docusaurus.config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: 'Flutter Unity Widget',
+  tagline: 'Embeddable unity game engine widget for Flutter',
+  url: 'https://flutter-unity.xraph.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'flutter-unity-widget', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Flutter Unity Widget',
+      logo: {
+        alt: 'Flutter Unity Widget',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://xraph.com/blog',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Get Started',
+              to: 'docs',
+            },
+            {
+              label: 'Android',
+              to: 'docs/android/getting-started/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/flutter-unity-widget',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/flutter-unity-widget',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/rex_raph',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/juicycleff/flutter-unity-widget',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} XRaph Ltd. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ sidebars.js
@@ -0,0 +1,10 @@
+module.exports = {
+  someSidebar: {
+    'Flutter Unity Widget': ['introduction', 'how-it-works'],
+    'Get Started': ['get-started/setup'],
+    'Unity': ['unity/setup'],
+    'Android': ['android/setup'],
+    'IOS': ['ios/setup'],
+    'Large Scale Project': ['support'],
+  },
+};
